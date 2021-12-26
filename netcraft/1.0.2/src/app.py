2a667c390e35d7f927435c6050925295b98cbf42
yi.liang@okg.com <yi.liang@okg.com>
2021-12-26 14:40:59 +0800

    bug测试

--- netcraft/1.0.2/src/app.py
+++ netcraft/1.0.2/src/app.py
@@ -36,31 +36,26 @@ class Netcraft(AppBase):
         auth = (user, password)
         return requests.post(url, auth=auth, headers=headers, data=data).text
 
-    def report_authorise(self, takedown_ids, cookie):
-        self.logger.error(type(takedown_ids))
-        self.logger.error(takedown_ids)
+    def report_authorise(self, takedown_id, cookie):
         result = True
-        return str(type(takedown_ids))
-        # for takedown_id in takedown_ids:
-        #     print(takedown_id)
-        #     url = "https://takedown.netcraft.com/ajax.php"
-        #     headers = {
-        #         "Cookie": cookie,
-        #         "Host": "takedown.netcraft.com",
-        #         "Content-Length": "92",
-        #         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-        #         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        #         "Accept": "text/javascript, text/html, application/xml, text/xml, */*",
-        #         "Origin": "https://takedown.netcraft.com",
-        #         "Referer": "https://takedown.netcraft.com/view.php?id="+str(takedown_id),
-        #         "Accept-Encoding": "gzip, deflate",
-        #     }
-        #     data = "auth_target=authorise&action=loadSection&section=summary&takedown_id="+str(takedown_id)+"&ajax_request=1"
-        #     res = requests.post(url, headers=headers, data=data)
-        #     if res.status_code != 200:
-        #         result = False
-        #         self.logger.error("报错了:"+str(res.status_code))
-        # return result
+        url = "https://takedown.netcraft.com/ajax.php"
+        headers = {
+            "Cookie": cookie,
+            "Host": "takedown.netcraft.com",
+            "Content-Length": "92",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Accept": "text/javascript, text/html, application/xml, text/xml, */*",
+            "Origin": "https://takedown.netcraft.com",
+            "Referer": "https://takedown.netcraft.com/view.php?id="+str(takedown_id),
+            "Accept-Encoding": "gzip, deflate",
+        }
+        data = "auth_target=authorise&action=loadSection&section=summary&takedown_id="+str(takedown_id)+"&ajax_request=1"
+        res = requests.post(url, headers=headers, data=data)
+        if res.status_code != 200:
+            result = False
+            self.logger.error("报错了:"+str(res.status_code))
+        return result
 
     # Can add a lot more to this
     def get_takedowns(self, user, password, id="", group_id="", url="", ip="", attack_url="", domain_attack="", statuses="", phishkit_only=""): 
